250667cd11ccab7d3e52a27dfe849a610a375b76
aniket-thakare <anirush123@gmail.com>
2022-02-17 15:08:12 +0530

    MadLib

---
+++ Madlib/aa.py
@@ -0,0 +1,13 @@
+
+
+def master():
+
+    funny_name = input("ENTER FUNNY NAME = ")
+    name = input("Enter any funy pet name =")
+    quality1 = input("Fully Quality =")
+    quality2 = input("Fully Quality 2 =")
+    mas = f"Hello, My name is {funny_name} and i am a {quality1}. well people call me {name} when they see me,\
+but i like {funny_name} very much.\
+when they call me like {quality2} i feel batter."
+    
+    print(mas)
---
+++ Madlib_Project.py
@@ -0,0 +1,20 @@
+# String concatination (aka how you put string togather)
+# creat string which says "subscribe to ___"
+# youtuber = "ZHC"
+
+# # few ways to do that
+
+# print("Subscribe to " + youtuber)
+# print("Subscribe to {}".format(youtuber))
+# print(f"Subscribe to {youtuber}")
+
+Easy = input("Enter Adjective = ")
+
+madlib = f"Computer programming is soo {Easy}!"
+
+print(madlib)
+
+from Madlib import aa
+
+if __name__ == "__main__":
+    aa.master()
